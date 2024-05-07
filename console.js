8e94906e56748816004ac64c4f1682effa8ed1a3
NIYUBAHWE Annick <147313167+annick975@users.noreply.github.com>
2024-05-07 10:53:12 -0700

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,7 @@
 // Hey! Just for learning purposes
 console.log("Here learning to merge pull requests")
 console.log("Here learning to merge pull requests") // 2nd
+function main() {
+  return null;
+}
+console.log(main);
